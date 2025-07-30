b2ffca30983688bf0c2d6d5e0262705c3a4172ca
Nipun <nipundhananjaya2021.com>
2025-07-30 22:53:02 +0530

    second commit

---
+++ test8.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int x = 10;
+    float f = 1.254;
+    char ch = 'A';
+
+    printf("Integer variable's value is : %d\n",x);
+    printf("Float variable\'s value is : %f\n",f);
+    printf("Character variables value is : %c\n",ch);
+
+
+
+   return 0;
+}
